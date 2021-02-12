d5f0ca496009ac66adecc4b9900d7966005f8a49
MichaelT4646 <mtambascio@quinnipiac.edu>
2021-02-12 14:02:04 -0500

    Testing to see if git works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test1212");
+    	double Pretzels;
+    	double GranolaBar;
+    	double Chips;
+    	double Soda;
+    	
+        System.out.println("Welcome to the vending machine! Here is a list of all of the snacks that are available to purchase!");
+        System.out.println("Pretzels: $2.00, Granola Bar: $1.00, Chips: $3.00, Soda: $2.00");
+        
     }
 
 }
